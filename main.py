87526c5896960ffb08ee00f632c4dc574f679787
parkash34 <ohm.parkash34@gmail.com>
2026-05-04 00:04:19 +0200

    Updated it

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel, field_validator
+from typing import Optional
+from langchain_groq import ChatGroq
