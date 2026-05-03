20c47b4d64a4b17c55b50bee3d7448b8b4389f39
parkash34 <ohm.parkash34@gmail.com>
2026-05-04 00:05:13 +0200

    Updated it

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel, field_validator
 from typing import Optional
 from langchain_groq import ChatGroq
+from langchain_core.tools import tool
+from langchain_core.messages import HumanMessage, AIMessage
+from langgraph.prebuilt import create_react_agent
