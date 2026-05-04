7ffb5e464eee5bc706df130b363dc0624e2e93a4
parkash34 <ohm.parkash34@gmail.com>
2026-05-04 15:06:15 +0200

    Updated it

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ from langchain_groq import ChatGroq
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage, AIMessage
 from langgraph.prebuilt import create_react_agent
+from database import DatabaseManager
+
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+if not api_key:
+    raise ValueError("API KEY is missing in env file")
+
+sessions = {}
+db_manager = DatabaseManager("restaurant.db")
+
 
